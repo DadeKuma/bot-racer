0dbcc5c7988bb97bafb98ad3f917551da629f14f
DadeKuma <mattafix68@gmail.com>
2023-07-11 02:29:02 +0200

    added sorting

--- src/components/LeaderboardTab.tsx
+++ src/components/LeaderboardTab.tsx
@@ -5,11 +5,14 @@ import { Contestant, RaceData, TabProps } from "../types";
 
 const LeaderboardTab: React.FC<TabProps> = ({ handleTabChange }) => {
     const [leaderboard, setLeaderboard] = useState<Contestant[]>([]);
+    const [sortedColumn, setSortedColumn] = useState<string>("USD");
+    const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
+    const columnHeadings = ["Bot", "USD", "Races", "Avg Position"];
 
     const formatUSD = (amount: number): string => {
-        const formatter = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
+        const formatter = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
             minimumFractionDigits: 2,
         });
         return formatter.format(amount);
@@ -36,6 +39,40 @@ const LeaderboardTab: React.FC<TabProps> = ({ handleTabChange }) => {
         }
     };
 
+    const sortColumn = (column: string) => {
+        if (sortedColumn === column) {
+            setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+        } else {
+            setSortOrder("asc");
+            setSortedColumn(column);
+        }
+    };
+
+    const leaderboardBody = () => {
+        return leaderboard.map((contestant, index) => {
+            const avgPosition =
+                contestant.positions.reduce((sum, position) => sum + position, 0) /
+                contestant.positions.length;
+            return (
+                <tr key={contestant.name}>
+                    <td>{index + 1}</td>
+                    <td>
+                        <Link
+                            to={`/bots/?search=${contestant.name}`}
+                            className={styles.botLink}
+                            onClick={() => handleTabChange("/bots")}
+                        >
+                            {contestant.name}
+                        </Link>
+                    </td>
+                    <td>{formatUSD(contestant.earnings)}</td>
+                    <td>{contestant.races}</td>
+                    <td>{avgPosition.toFixed(1)}</td>
+                </tr>
+            );
+        });
+    };
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -56,55 +93,75 @@ const LeaderboardTab: React.FC<TabProps> = ({ handleTabChange }) => {
                         addToContestant(contestantsMap, name, prize.B, 1 + winner.length + A.length + idx)
                     );
                     C.forEach((name, idx) =>
-                        addToContestant(contestantsMap, name, 0, 1 + winner.length + A.length + B.length + idx));
+                        addToContestant(contestantsMap, name, 0, 1 + winner.length + A.length + B.length + idx)
+                    );
                 });
 
                 const contestants: Contestant[] = Array.from(contestantsMap.values());
-
-                contestants.sort((a, b) => b.earnings - a.earnings);
                 setLeaderboard(contestants);
             } catch (error) {
                 console.error("Error fetching race data:", error);
             }
         };
+
         fetchData();
     }, []);
+
+    useEffect(() => {
+        const sortedLeaderboard = [...leaderboard].sort((a, b) => {
+            if (sortedColumn === "Bot") {
+                return sortOrder === "asc"
+                    ? a.name.localeCompare(b.name)
+                    : b.name.localeCompare(a.name);
+            } else if (sortedColumn === "USD") {
+                return sortOrder === "asc"
+                    ? a.earnings - b.earnings
+                    : b.earnings - a.earnings;
+            } else if (sortedColumn === "Races") {
+                return sortOrder === "asc"
+                    ? a.races - b.races
+                    : b.races - a.races;
+            } else if (sortedColumn === "Avg Position") {
+                const avgPositionA =
+                    a.positions.reduce((sum, position) => sum + position, 0) /
+                    a.positions.length;
+                const avgPositionB =
+                    b.positions.reduce((sum, position) => sum + position, 0) /
+                    b.positions.length;
+                return sortOrder === "asc"
+                    ? avgPositionA - avgPositionB
+                    : avgPositionB - avgPositionA;
+            }
+            return 0;
+        });
+
+        setLeaderboard(sortedLeaderboard);
+    }, [sortedColumn, sortOrder, leaderboard]);
 
     return (
         <div className={styles.leaderboardTab}>
             <table className={styles.leaderboardTable}>
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>Bot</th>
-                        <th>USD</th>
-                        <th>Races</th>
-                        <th>Avg Position</th>
+                        <th>Rank</th>
+                        {columnHeadings.map((column) => (
+                            <th
+                                key={column}
+                                onClick={() => sortColumn(column)}
+                                className={sortedColumn === column ? sortOrder : ""}
+                            >
+                                {column}
+                                {sortedColumn === column && (
+                                    <span className={`arrow ${sortOrder === "asc" ? "asc" : "desc"}`}>
+                                        {sortOrder === "asc" ? "▲" : "▼"}
+                                    </span>
+                                )}
+                            </th>
+                        ))}
                     </tr>
                 </thead>
                 <tbody>
-                    {leaderboard.map((contestant, index) => {
-                        const avgPosition =
-                            contestant.positions.reduce((sum, position) => sum + position, 0) /
-                            contestant.positions.length;
-                        return (
-                            <tr key={contestant.name}>
-                                <td>{index + 1}</td>
-                                <td>
-                                    <Link
-                                        to={`/bots/?search=${contestant.name}`}
-                                        className={styles.botLink}
-                                        onClick={() => handleTabChange("/bots")}
-                                    >
-                                        {contestant.name}
-                                    </Link>
-                                </td>
-                                <td>{formatUSD(contestant.earnings)}</td>
-                                <td>{contestant.races}</td>
-                                <td>{avgPosition.toFixed(1)}</td>
-                            </tr>
-                        );
-                    })}
+                    {leaderboardBody()}
                 </tbody>
             </table>
         </div>
