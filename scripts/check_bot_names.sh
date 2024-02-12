de3a1923e8214008eae749f23f7d0eae3d7c33c7
DadeKuma <mattafix68@gmail.com>
2024-02-12 21:29:57 +0100

    added auto check script

---
+++ scripts/check_bot_names.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+for file in public/data/races/*.json; do
+    # Read JSON file
+    data=$(<"$file")
+
+    # Read valid_names.json
+    valid_bot_names=$(<public/data/bots.json)
+
+    # Extract bot_names from data.json
+    bot_names=$(echo "$data" | jq -r '.[].data | .winner[], .A[], .B[], .C[]')
+
+    # Check if all bot_names are valid
+    invalid_names=""
+    for name in $bot_names; do
+        if ! echo "$valid_bot_names" | grep -q "$name"; then
+            invalid_names+=" $name"
+        fi
+    done
+
+    if [ -n "$invalid_names" ]; then
+        echo "Invalid bot_names found in $file: $invalid_names"
+        exit 1
+    else
+        echo "All bot_names in $file are valid"
+    fi
+done
